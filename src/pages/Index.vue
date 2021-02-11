da01fe0f38ca68c7611a121825493856977ffd5a
Anne-Sophie Tranchet <anne-sophie.tranchet@beta.gouv.fr>
2021-02-11 16:44:16 +0100

    WIP for the service page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,51 @@
 <template>
   <Layout>
+    <div class="rf-container">
+        <div class="rf-grid-row">
+            <div class="rf-col">
+            <h1>Concevons des services publics numériques accessibles, inclusifs et humains.</h1>
+            <h2>Des ressources à la disposition des membres de la communauté beta.gouv.fr pour améliorer l'expérience usager des services publics numériques.</h2>
+	        </div>
+	    </div>
 
+        <div class="rf-grid-row">
+            <div class="rf-col">
+	            <h2>Accompagnement</h2>
 
+	            <p>avec lien vers un formulaire</p>
 
+	            <h2>Organisation d’ateliers</h2>
+
+				<div class="rf-grid-row rf-grid-row--gutters">
+                    <div class="rf-col-12 rf-col-md-4">
+                         <a class="rf-card" href="#">
+                            <div class="rf-card__img"><img src="https://dl.airtable.com/.attachments/726f846f7fefcd86fada7786d46244ea/789d48cd/atelier-design.svg">
+                            </div>
+                            <div class="rf-card__body">
+                                <h2 class="rf-card__title">Landing Page</h2>
+                                <p class="rf-card__desc">Co-concevez la page d'accueil de votre service</p>
+                            </div>
+                         </a>
+                    </div>
+                </div>
+
+	            <h2>Pour les designers</h2>
+
+	            <p>Design system
+							Documentation
+							Vous êtes nouveau contactez-nous !
+				</p>
+			</div>
+        </div>
+    </div>
   </Layout>
 </template>
+
+<script>
+export default {
+
+  metaInfo: {
+    title: "Designer Transverses - Le design au service des startups d'État"
+  }
+}
+</script>
