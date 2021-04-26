c1b2c6f6e081da5b958f46d3fdca354a13b0de26
Jeremie Cook <hello@jeremiecook.com>
2021-04-26 19:26:50 +0200

    header review

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,51 +1,59 @@
 <template>
-  <header class="fr-header">
-    <div class="fr-container">
-      <div class="fr-header__body">
-        <div class="fr-header__brand">
-          <span class="fr-logo">
-            <span class="fr-logo__title"
-              >République
-              <br />
-              française</span
-            >
-          </span>
-        </div>
-        <div class="fr-header__navbar">
-          <div class="fr-service">
-            <a class="fr-service__title" href="/">
-              {{ service_name }}
-            </a>
-            <p class="fr-service__tagline">de l'incubateur beta.gouv.fr</p>
+  <header role="banner" class="fr-header">
+    <div class="fr-header__body">
+      <div class="fr-container">
+        <div class="fr-header__body-row">
+          <div class="fr-header__brand fr-enlarge-link">
+            <div class="fr-header__brand-top">
+              <div class="fr-header__logo">
+                <a href="/" title="Accueil - Designers transverses">
+                  <p class="fr-logo">
+                    République
+                    <br />Française
+                  </p>
+                </a>
+              </div>
+            </div>
+            <div class="fr-header__service">
+              <a href="/" title="Accueil - Designers transverses">
+                <p class="fr-header__service-title">{{ service_name }}</p>
+              </a>
+              <p class="fr-header__service-tagline">
+                de l'incubateur beta.gouv.fr
+              </p>
+            </div>
           </div>
         </div>
+        <nav
+          id="header-navigation"
+          class="fr-nav"
+          role="navigation"
+          aria-label="Menu principal"
+        >
+          <ul class="fr-nav__list">
+            <li class="fr-nav__item">
+              <g-link class="fr-nav__link" to="/" target="_self"
+                >Services</g-link
+              >
+            </li>
+            <li class="fr-nav__item">
+              <g-link class="fr-nav__link" to="/events" target="_self"
+                >Évènements</g-link
+              >
+            </li>
+            <li class="fr-nav__item">
+              <g-link class="fr-nav__link" to="/guides" target="_self"
+                >Guides</g-link
+              >
+            </li>
+            <li class="fr-nav__item">
+              <g-link class="fr-nav__link" to="/stats" target="_self"
+                >Statistiques</g-link
+              >
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <nav
-        id="header-navigation"
-        class="fr-nav"
-        role="navigation"
-        aria-label="Menu principal"
-      >
-        <ul class="fr-nav__list">
-          <li class="fr-nav__item">
-            <g-link class="fr-link" to="/" target="_self">Services</g-link>
-          </li>
-          <li class="fr-nav__item">
-            <g-link class="fr-link" to="/events" target="_self"
-              >Évènements</g-link
-            >
-          </li>
-          <li class="fr-nav__item">
-            <g-link class="fr-link" to="/guides" target="_self">Guides</g-link>
-          </li>
-          <li class="fr-nav__item">
-            <g-link class="fr-link" to="/stats" target="_self"
-              >Statistiques</g-link
-            >
-          </li>
-        </ul>
-      </nav>
     </div>
   </header>
 </template>
@@ -64,7 +72,7 @@ export default {
 </script>
 
 <style lang="scss">
-.fr-link {
+.fr-nav__link {
   &.active--exact {
     color: var(--bf500);
     box-shadow: inset 0 -2px var(--bf500);
