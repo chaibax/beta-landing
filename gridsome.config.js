5dc48c3e9d9db610bee1694eda3cb0a6e30e0746
Chaïb Martinez <chaib@zyyne.com>
2021-05-03 22:15:51 +0200

    wip

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: 'Landing Page Builder beta.gouv',
+    siteName: 'Landing Page Builder',
    
     transformers: {
         remark: {
